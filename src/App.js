6e754767ce2f7acde9588de23f35f86b79208b24
Fathima-Fidha <fathimafidha2903@gmail.com>
2024-12-23 15:57:36 +0530

    netflix

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect  } from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from './Components/Login/Login';
+import NavBar from './Components/NavBar/NavBar'
+import {action,originals} from './urls'
+import './App.css'
+import Banner from './Components/Banner/Banner'
+import RowPost from './Components/RowPost/RowPost'
+function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  
+  useEffect(() => {
+    const authStatus = localStorage.getItem("isAuthenticated");
+    if (authStatus === "true") {
+      setIsAuthenticated(true);
+    }
+  }, []);
+  
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/login" element={
+             isAuthenticated ? (<Navigate to="/home" />) : (<Login setIsAuthenticated={setIsAuthenticated} /> )
+            }/>
+          
+          {/* Protected Route: Dashboard */}
+          <Route path="/home" element={
+              isAuthenticated ? (
+                <>
+                  <NavBar setIsAuthenticated={setIsAuthenticated} />
+                  <Banner />
+                  <RowPost url={originals} title="Netflix Originals" />
+                  <RowPost url={action} title="Action" isSmall />
+                </>
+              ) : ( <Navigate to="/login" />)
+            }/>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+
+export default App
---
+++ src/constants/constants.js
@@ -0,0 +1,4 @@
+export const baseUrl ='https://api.themoviedb.org/3';
+
+export const API_KEY='914f44e70c9d1bbb6c3334bb9a2b8e0a';
+export const imageUrl ='https://image.tmdb.org/t/p/original';
